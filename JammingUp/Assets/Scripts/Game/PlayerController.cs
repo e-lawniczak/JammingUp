18511b0211d7de1c621411f1721db5fb2910043d
e-lawniczak <60924751+e-lawniczak@users.noreply.github.com>
2023-07-08 11:50:38 +0200

    Update PlayerController.cs

--- JammingUp/Assets/Scripts/Game/PlayerController.cs
+++ JammingUp/Assets/Scripts/Game/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public Transform movePoint;
+    private bool axisXInUse = false; 
+    private bool axisYInUse = false;
 
     void Start()
     {
@@ -33,19 +35,42 @@ public class PlayerController : MonoBehaviour
         float inputH = Input.GetAxisRaw("Horizontal");
         float inputV = Input.GetAxisRaw("Vertical");
 
-        if (Vector3.Distance(transform.position, movePoint.position) >= .02f)
+        if (Vector3.Distance(transform.position, movePoint.position) >= .05f)
             // if the player didnt' reach the point yet - prevent moving 
             return;
 
         if (Mathf.Abs(inputH) == 1f)
         {
             // detect arrow left or arrow right
-            movePoint.position += new Vector3(inputH, 0f, 0f);
+            if (!axisXInUse)
+            {
+                movePoint.position += new Vector3(inputH, 0f, 0f);
+                // prevent hold down button movement
+                axisXInUse = true;
+            }
         }
         else if (Mathf.Abs(inputV) == 1f)
         {
-            // detect arrow up or arrow down
-            movePoint.position += new Vector3(0f, inputV, 0f);
+            if (!axisYInUse)
+            {
+                // detect arrow up or arrow down
+                movePoint.position += new Vector3(0f, inputV, 0f);
+                // prevent hold down button movement
+                axisYInUse = true;
+
+            }
+        }
+
+        //unlock movement after ButtonUpEvent
+        if (Mathf.Abs(inputH) == 0f)
+        {
+            axisXInUse = false;
+
+        }
+        if (Mathf.Abs(inputV) == 0f)
+        {
+            axisYInUse = false;
+
         }
     }
 }
